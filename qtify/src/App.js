d59f6a6adac0cf267f40763badea91720f2f8404
Rahul-1904 <rahulkumar19042000@gmail.com>
2025-03-22 22:29:41 +0530

    Card implementation commit

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -11,6 +11,7 @@ function App() {
       <Hero/>
       {/* <SongCard albumName={"New Bollywood"} follows={100}/> */}
       <Section title="Top Albums" apiEndpoint="https://qtify-backend-labs.crio.do/albums/top" />
+      <Section title="New Albums" apiEndpoint="https://qtify-backend-labs.crio.do/albums/top" />
     </div>
   );
 }
