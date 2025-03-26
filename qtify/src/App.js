d10ba46ccd1502120e42a7a9ac1b793b3f1ce73c
Rahul-1904 <rahulkumar19042000@gmail.com>
2025-03-26 17:52:48 +0530

    top album api changed

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Hero/>
       {/* <SongCard albumName={"New Bollywood"} follows={100}/> */}
       <Section title="Top Albums" apiEndpoint="https://qtify-backend-labs.crio.do/albums/top" />
-      <Section title="New Albums" apiEndpoint="https://qtify-backend-labs.crio.do/albums/top" />
+      <Section title="New Albums" apiEndpoint="https://qtify-backend-labs.crio.do/albums/new" />
       <Songs/>
     </div>
   );
